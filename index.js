d7c352bc97f4102a7d906e00e0dde2092f834e43
suman <sumobarnwal@gmail.com>
2024-01-06 14:02:06 +0530

    req and res by path

---
+++ index.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const url = require('url');
+
+const server = http.createServer((req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+
+    if (parsedUrl.pathname === '/') {
+        console.log('data sent by browser =>', parsedUrl.query.name);
+        res.end('Welcome to the home page');
+    } else if (parsedUrl.pathname === '/about') {
+         res.end('Welcome to the about page');
+    } else if (parsedUrl.pathname === '/help') {
+         res.end('Welcome to the help page');
+    } else {
+        res.end('Not Found');
+    }
+});
+
+server.listen(3000, () => {
+    console.log('Server is running on port');
+});
